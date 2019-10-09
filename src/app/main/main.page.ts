46424c03e59cac0c2e53fe19feab8ace13790db7
Foenix <max@kadyrov.me>
2019-10-09 18:12:01 +0600

    fix

--- src/app/main/main.page.ts
+++ src/app/main/main.page.ts
@@ -7,9 +7,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPage implements OnInit {
 
+  moreShow: any = true;
+  cvs: any = Object;
+  ctx: any = Object;
+  w: any = Number;
+  h: any = Number;
+  x: any = Number;
+  y: any = Number;
+  d: any = Number;
+  dm: any = Number;
+  j: any = Number;
+  w1: any = Number;
+  h1: any = Number;
+  X: any = Array;
+  Y: any = Array;
+  C: any = Array;
+  i: any = Number;
+  n: any = 20;
+  b: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.cvs = document.getElementById("cvsId");
+    this.ctx = this.cvs.getContext("2d");
+    this.w = this.cvs.width;
+    this.h = this.cvs.height;
+    this.x = 0;
+    this.y = 0;
+    this.d = 0;
+    this.dm = 0;
+    this.j=0;
+    this.w1 = this.w - 2;
+    this.h1 = this.h - 2;
+    this.X = new Array(this.n);
+    this.Y = new Array(this.n);
+    this.C = new Array(this.n);
+    for (this.i = 0; this.i < this.n; this.i++) {
+      this.X[this.i] = this.randgp(this.w1);
+      this.Y[this.i] = this.randgp(this.h1);
+      this.C[this.i] = this.randhclr();
+    }
+    console.log(this.C);
+    for (this.y = 0; this.y < this.h1; this.y++) {
+      for (this.x = 0; this.x < this.w1; this.x++) {
+        this.b = false;
+        this.dm = this.metric(this.h1, this.w1, 1);
+        this.j = this.j - 1;
+        for (this.i = 0; this.i < this.n; this.i++) {
+           this.d = this.metric(this.X[this.i] - this.x,this.Y[this.i] - this.y, 1)
+           if (this.d < this.dm) {
+             this.dm = this.d;
+             this.j = this.i;
+             this.b = false;
+           } else {
+             this.b = true;
+           }
+        }
+        if (this.b) {
+          this.ctx.fillStyle = '#FFF';
+        } else {
+          this.ctx.fillStyle = this.C[this.j];
+        }
+  			this.ctx.fillRect(this.x , this.y , 1, 1);
+      }
+    }
   }
 
+  randgp(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  randhclr() {
+    return "#444"/*+
+    ("00"+this.randgp(256).toString(16)).slice(-2)+
+    ("00"+this.randgp(256).toString(16)).slice(-2)+
+    ("00"+this.randgp(256).toString(16)).slice(-2);
+    */
+  }
+
+  metric(x , y, mt) {
+    if(mt==1) {return Math.sqrt(x*x + y*y)}
+    if(mt==2) {return Math.abs(x) + Math.abs(y)}
+    if(mt==3) {return(Math.pow(Math.pow(Math.abs(x),3) + Math.pow(Math.abs(y),3),0.33333))}
+  }
 }
