53c05267ae8b52a034845908e89e499924d610f8
Max <max@kadyrov.me>
2019-11-04 17:34:30 +0600

    f

--- src/app/explore/explore.page.ts
+++ src/app/explore/explore.page.ts
@@ -1,161 +1,204 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ActionSheetController, ModalController } from '@ionic/angular';
-import { ModalPage } from '../modal/modal.page';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {ActionSheetController, ModalController} from '@ionic/angular';
+import {ModalPage} from '../modal/modal.page';
+import {environment} from '../../environments/environment';
+import * as _ from 'lodash';
+import {HttpClient} from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import {Storage} from '@ionic/storage';
 
 @Component({
-  selector: 'app-explore',
-  templateUrl: './explore.page.html',
-  styleUrls: ['./explore.page.scss'],
+    selector: 'app-explore',
+    templateUrl: './explore.page.html',
+    styleUrls: ['./explore.page.scss'],
 })
 export class ExplorePage implements OnInit {
 
-  task: any;
-  defTasks: any;
-  strings: any;
-  days: any;
+    task: any;
+    defTasks: any;
+    strings: any;
+    days: any;
 
-  time: any;
-  rand: any;
-  animate: any = false;
-  taskTrue: any = false;
+    time: any;
+    rand: any;
+    animate: any = false;
+    taskTrue: any = false;
 
-  boneDef: any = 1;
-  boneCount: any = 1;
-  boneDisabled: any = true;
-  boneInterval = 500;
-  boneTimeout: any = 4000;
+    boneDef: any = 1;
+    boneCount: any = 1;
+    boneDisabled: any = true;
+    boneInterval = 500;
+    boneTimeout: any = 4000;
 
-  boneTextDef = 1;
-  boneTextCount = 0;
-  boneTextDisabled: any = false;
-  boneTextInterval: any = 500;
-  boneTextTimeout: any = 4000;
+    boneTextDef = 1;
+    boneTextCount = 0;
+    boneTextDisabled: any = false;
+    boneTextInterval: any = 500;
+    boneTextTimeout: any = 4000;
 
-  valueTask: any = '';
-  valueView: any = false;
+    valueTask: any = '';
+    valueView: any = false;
+    goal: any;
 
-  constructor(
-    public router: Router,
-    public actionSheetController: ActionSheetController,
-    public modalController: ModalController
-  ) {
-    this.defTasks = [{text: 'Task 1', role: '1', checked: false},{text: 'Task 2', role: '2', checked: false},{text: 'Task 3', role: '3', checked: false}];
-    this.task = {type: {name: 'Goal', checked: false}, rate: '', task: {}};
-    this.days = [{value: 1},{value: 2},{value: 3},{value: 4},{value: 5},{value: 6}];
-    this.strings = [{value: 1, name: 'Learn'},{value: 2, name: 'Refine'},{value: 3, name: 'Action'},{value: 4, name: 'Social'},{value: 5, name: 'Fun'},{value: 6, name: 'Wild Card'}];
-  }
-
-  ngOnInit() {
-    this.defTasks.push({text: 'Add My Own', role: '2'})
-  }
-
-  randMath (bone) {
-    const rand = Math.floor(1 + Math.random() * (6 + 1 - 1));
-    if (rand == bone) {
-      return this.randMath(bone);
-    } else {
-      return rand;
+    constructor(
+        public router: Router,
+        public storage: Storage,
+        public http: HttpClient,
+        public actionSheetController: ActionSheetController,
+        public modalController: ModalController
+    ) {
+        // this.defTasks = [{text: 'Task 1', role: '1', checked: false}, {text: 'Task 2', role: '2', checked: false}, {
+        //     text: 'Task 3',
+        //     role: '3',
+        //     checked: false
+        // }];
+        this.task = {type: {name: 'Goal', checked: false}, rate: '', task: {}};
+        this.days = [{value: 1}, {value: 2}, {value: 3}, {value: 4}, {value: 5}, {value: 6}];
+        this.strings = [{value: 1, name: 'Learn'}, {value: 2, name: 'Refine'}, {value: 3, name: 'Action'}, {
+            value: 4,
+            name: 'Social'
+        }, {value: 5, name: 'Fun'}, {value: 6, name: 'Wild Card'}];
     }
-  }
 
-  clickBoneText () {
-    const that = this;
-    if (that.boneTextDisabled == false) {
-      that.animate = true;
-      that.boneTextDisabled = true;
+    ngOnInit() {
+        const headers = new Headers();
+        headers.append('Accept', 'application/json');
+        headers.append('Content-Type', 'application/json');
+        this.http.get(environment.url + '/goalList', {})
+            .subscribe((data: any) => {
+                const g = _.find(data, ['category', 'Refine']);
+                this.defTasks = g.tasks.map((task) => {
+                    task.text = task.name;
+                    task.role = task.name;
+                    return task;
+                });
+                this.defTasks.push({text: 'Add My Own', role: '2'});
 
-      that.time = setInterval(function(){
-        that.boneTextCount = that.randMath(that.boneTextCount);
-      }, that.boneTextInterval);
+                console.log('d', this.defTasks);
+            }, errorResp => {
+                console.log(errorResp);
+            });
 
-      setTimeout(function(){
-        clearInterval(that.time);
-        that.boneTextCount = that.boneTextDef;
-        that.animate = false;
-        that.taskTrue = true;
-        that.boneDisabled = false;
-      }, that.boneTextTimeout);
+        this.http.get(environment.url + '/positions', {})
+            .subscribe((data: any) => {
+                let that = this;
+                this.storage.get('jobId').then((jobId) => {
+                    that.goal = data.find((position) => position.id === jobId);
+                    console.log(that.goal);
+                });
+
+            }, errorResp => {
+                console.log(errorResp);
+            });
     }
-  }
 
-  clickBone () {
-    const that = this;
-    if (that.boneDisabled == false) {
-      that.animate = true;
-      that.boneDisabled = true;
-      that.time = setInterval(function(){
-        that.boneCount = that.randMath(that.boneCount);
-      }, that.boneInterval);
-      setTimeout(function(){
-        clearInterval(that.time);
-        that.boneCount = that.boneCount;
-        that.animate = false;
-      }, that.boneTimeout);
-    }
-  }
-
-  rateSet(rate) {
-    this.task.rate = rate;
-  }
-
-  addTask() {
-    if (this.valueTask != '') {
-      this.task.task = {text: this.valueTask, role: 1, checked: false};
-    }
-  }
-
-  async presentActionSheet() {
-    const that = this;
-    const actionSheet = await this.actionSheetController.create({
-      header: 'Select One',
-      buttons: this.defTasks,
-    });
-    await actionSheet.present();
-    actionSheet.onWillDismiss().then((response:any) => {
-      this.clickBone();
-      this.defTasks.forEach(function(item){
-        if (item.role == 2) {
-          that.valueView = true;
-          that.taskTrue = false;
+    randMath(bone) {
+        const rand = Math.floor(1 + Math.random() * (6 + 1 - 1));
+        if (rand == bone) {
+            return this.randMath(bone);
         } else {
-          if (item.role == response.role) {
-            that.task.task = {text: item.text, role: item.role, checked: false};
-          }
+            return rand;
         }
-      });
-    });
-  }
+    }
 
-  async openModal() {
-    const modal: HTMLIonModalElement = await this.modalController.create({
-      component: ModalPage
-    });
+    clickBoneText() {
+        const that = this;
+        if (that.boneTextDisabled == false) {
+            that.animate = true;
+            that.boneTextDisabled = true;
 
-    modal.onDidDismiss().then((result) => {
-      console.log(result);
-    });
+            that.time = setInterval(function() {
+                that.boneTextCount = that.randMath(that.boneTextCount);
+            }, that.boneTextInterval);
 
-    return await modal.present();
-  }
+            setTimeout(function() {
+                clearInterval(that.time);
+                that.boneTextCount = that.boneTextDef;
+                that.animate = false;
+                that.taskTrue = true;
+                that.boneDisabled = false;
+            }, that.boneTextTimeout);
+        }
+    }
 
-  home () {
-    this.router.navigate(['/main']);
-  }
+    clickBone() {
+        const that = this;
+        if (that.boneDisabled == false) {
+            that.animate = true;
+            that.boneDisabled = true;
+            that.time = setInterval(function() {
+                that.boneCount = that.randMath(that.boneCount);
+            }, that.boneInterval);
+            setTimeout(function() {
+                clearInterval(that.time);
+                that.boneCount = that.boneCount;
+                that.animate = false;
+            }, that.boneTimeout);
+        }
+    }
 
-  explore () {
-    this.router.navigate(['/explore']);
-  }
+    rateSet(rate) {
+        this.task.rate = rate;
+    }
 
-  collect () {
-    this.router.navigate(['/collect']);
-  }
+    addTask() {
+        if (this.valueTask != '') {
+            this.task.task = {text: this.valueTask, role: 1, checked: false};
+        }
+    }
 
-  user () {
-    this.router.navigate(['/user']);
-  }
+    async presentActionSheet() {
+        const that = this;
+        const actionSheet = await this.actionSheetController.create({
+            header: 'Select One',
+            buttons: this.defTasks,
+        });
+        await actionSheet.present();
+        actionSheet.onWillDismiss().then((response: any) => {
+            this.clickBone();
+            this.defTasks.forEach(function(item) {
+                if (item.role == 2) {
+                    that.valueView = true;
+                    that.taskTrue = false;
+                } else {
+                    if (item.role == response.role) {
+                        that.task.task = {text: item.text, role: item.role, checked: false};
+                    }
+                }
+            });
+        });
+    }
 
-  more () {
-    this.router.navigate(['/more']);
-  }
+    async openModal() {
+        const modal: HTMLIonModalElement = await this.modalController.create({
+            component: ModalPage
+        });
+
+        modal.onDidDismiss().then((result) => {
+            console.log(result);
+        });
+
+        return await modal.present();
+    }
+
+    home() {
+        this.router.navigate(['/main']);
+    }
+
+    explore() {
+        this.router.navigate(['/explore']);
+    }
+
+    collect() {
+        this.router.navigate(['/collect']);
+    }
+
+    user() {
+        this.router.navigate(['/user']);
+    }
+
+    more() {
+        this.router.navigate(['/more']);
+    }
 }
